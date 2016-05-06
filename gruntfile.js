39f1533ad1d7b621bd39319ec0c72bd0151ebdff
mark.rudman <mark@boyofbeard.com>
2016-05-06 12:22:05 +0100

    Initial Commit
    First commit with core folder structure in place.

---
+++ gruntfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = function(grunt){
+
+    var modules = [
+        'grunt-contrib-jshint',
+        'grunt-contrib-sass',
+        'grunt-cssnano',
+        'grunt-postcss',
+        'grunt-contrib-watch'
+    ];
+
+    modules.forEach(function(task){
+        grunt.loadNpmTasks(task);
+    });
+
+    require('time-grunt')(grunt);
+
+     grunt.initConfig({
+         jshint:{
+             all: ['js/*.js']
+         },
+         sass:{
+             dist: {
+                 options: {
+                     style: 'expanded',
+                     sourcemap: 'none'
+                 },
+                 files: {
+                     'css/style-expanded.css': 'scss/main.scss'
+                 }
+             }
+         },
+         postcss: {
+             options: {
+                 map: {
+                     inline: false, // save all sourcemaps as separate files...
+                     annotation: 'css' // ...to the specified directory
+                 },
+
+                 processors: [
+                     require('pixrem')(), // add fallbacks for rem units
+                     require('autoprefixer')({
+                         browsers: ['not ie < 8']
+                     })
+                 ]
+             },
+             dist: {
+                 src: 'css/style-expanded.css'
+             }
+         },
+         cssnano: {
+             options: {
+                 sourcemap: false
+             },
+             dist: {
+                 files: {
+                     'css/style-min.css': 'css/style-expanded.css'
+                 }
+             }
+         },
+         watch: {
+             scripts: {
+                 files: ['scss/*.scss' ],
+                 tasks: ['sass', 'postcss:dist', 'cssnano'],
+                 options: {
+                     livereload: true,
+                     spawn: false
+                 }
+             }
+         }
+     });
+
+
+    grunt.registerTask('js', ['jshint']);
+    grunt.registerTask('css', ['sass', 'postcss:dist', 'cssnano']);
+    grunt.registerTask('default', ['sass', 'postcss:dist', 'cssnano', 'watch']);
+
+};
---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function(){
+
+	var body = document.body,
+		rootContainer = document.getElementById('js-root'),
+		mainMenu = document.getElementById('js-main_nav'),
+		mainMenuIcon = document.getElementById('js-main_nav_icon'),
+		mainMenuClose = document.getElementById('js-main_nav_close_icon');
+
+	mainMenuIcon.addEventListener('click', function(event){
+		event.preventDefault();
+		mainMenu.classList.toggle('main_nav-active');
+		body.classList.toggle('no_scroll-sm_only');
+	});
+
+	mainMenuClose.addEventListener('click', function(event){
+		event.preventDefault();
+		mainMenu.classList.remove('main_nav-active');
+		body.classList.toggle('no_scroll-sm_only');
+	});
+
+})();

90fb266e24a30f569190be56173c655fae3a81b9
Mark <Ruds@Marks-MacBook-Pro.local>
2016-05-14 00:03:42 +0100

    Images added to scrapbook page

--- js/main.js
+++ js/main.js
@@ -8,17 +8,17 @@
 		mainMenuIcon = document.getElementById('js-main_nav_icon'),
 		mainMenuClose = document.getElementById('js-main_nav_close_icon');
 
-	mainMenuIcon.addEventListener('click', function(event){
-		event.preventDefault();
-		mainMenu.classList.toggle('main_nav-active');
-		body.classList.toggle('no_scroll-sm_only');
-	});
+		mainMenuIcon.addEventListener('click', function(event){
+			event.preventDefault();
+			mainMenu.classList.toggle('main_nav-active');
+			body.classList.toggle('no_scroll-sm_only');
+		});
 
-	mainMenuClose.addEventListener('click', function(event){
-		event.preventDefault();
-		mainMenu.classList.remove('main_nav-active');
-		body.classList.toggle('no_scroll-sm_only');
-	});
+		mainMenuClose.addEventListener('click', function(event){
+			event.preventDefault();
+			mainMenu.classList.remove('main_nav-active');
+			body.classList.toggle('no_scroll-sm_only');
+		});
 
 	/*** SCRAPBOOK ***********************/
 
@@ -27,17 +27,36 @@
 		scrapbookImages = document.getElementsByClassName('scrapbook_img'),
 		scrapbookImageContainer = document.getElementById('js-scrapbook_img_container');
 
+		var clickable = true;
 
-	scrapbookClose.addEventListener('click', function(event){
-		event.preventDefault();
-		scrapbookContainer.classList.toggle('scrapbook_container-is_active');
-	});
 
-	for( var i = 0; i < scrapbookImages.length; i++ ){
-		scrapbookImages[i].addEventListener('click', function(event){
+		scrapbookClose.addEventListener('click', function(event){
 			event.preventDefault();
 			scrapbookContainer.classList.toggle('scrapbook_container-is_active');
+			scrapbookImageContainer.style.transform = null;
+			clickable = true;
 		});
-	}
+
+
+
+		for( var i = 0; i < scrapbookImages.length; i++ ){
+			scrapbookImages[i].addEventListener('click', listener.bind(null, i), false );
+		}
+
+
+		function listener(index) {
+		    event.preventDefault();
+
+			var translateValue = 'translateX(' + scrapbookImages[index].getAttribute('data-order') * -100 + '%)';
+			if( clickable ){
+				scrapbookContainer.classList.toggle('scrapbook_container-is_active');
+				scrapbookImageContainer.style.transform = translateValue;
+				clickable = false;
+			}
+
+		}
+
+
+
 
 })();

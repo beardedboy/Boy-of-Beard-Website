f4d99053993fcfc60ce8322629aba5d7fbec09c1
Mark <Ruds@Marks-MacBook-Pro.local>
2016-05-23 21:58:02 +0100

    gallery updates

--- js/main.js
+++ js/main.js
@@ -31,18 +31,79 @@
 	*
 	******************************************************/
 
-	var modalBG = document.getElementById('js-modal_bg'),
+	var modal = document.getElementById('js-modal'),
+		modalMainContent = document.getElementById('js-modal_main_content'),
+
+		//Clickable elements
+		modalBG = document.getElementById('js-modal_bg'),
 		modalCloseBtn = document.getElementById('js-modal_close'),
-		modal = document.getElementById('js-modal'),
-		modalMainContent = document.getElementById('js-modal_main_content');
+		scrapbookImages = document.getElementsByClassName('scrapbook_img'),
+		scrapbookImagesTotal = scrapbookImages.length;
+
+		modalImageContainer = document.getElementById('js-modal_gallery_container'),
+		currentImageTitle = document.getElementById('js-modal_main_sidebar_content_title'),
+		currentImageDesc = document.getElementById('js-modal_main_sidebar_content_desc'),
+
+		previousBtn = document.getElementById('js-modal_main_content-previous_button'),
+		nextBtn = document.getElementById('js-modal_main_content-next_button');
 
 
-	modalBG.addEventListener( 'click', toggleModalVisibility,false );
-	modalCloseBtn.addEventListener( 'click', toggleModalVisibility,false );
 
-	function toggleModalVisibility(event){
+	modalBG.addEventListener( 'click', toggleModalHide,false );
+	modalCloseBtn.addEventListener( 'click', toggleModalHide,false );
+
+
+	var frag = document.createDocumentFragment();
+
+	for( var i = 0; i < scrapbookImages.length; i++ ){
+			scrapbookImages[i].addEventListener('click', toggleModalVisible.bind(null, i), false );
+
+			var span = document.createElement("span");
+			span.classList.add('modal_img_container');
+			frag.appendChild(span);
+
+		}
+
+	modalImageContainer.appendChild(frag);
+
+	function toggleModalHide(event){
 		event.preventDefault();
-		modal.classList.toggle('modal-active');
+		modal.classList.remove('modal-active');
+		body.classList.remove('no_scroll');
+	}
+
+
+	function toggleModalVisible(index){
+		event.preventDefault();
+
+		var order = scrapbookImages[index].getAttribute('data-order'),
+			title = scrapbookImages[index].getAttribute('data-title'),
+			desc = scrapbookImages[index].getAttribute('data-desc'),
+			containers = document.getElementsByClassName('modal_img_container');
+
+		//Injects the active image into the DOM 
+		var newImg = document.createElement('img');
+		newImg.classList.add('modal_main_img');
+		newImg.src = "img/scrapbook/image"+ order + ".jpg";
+		newImg.alt = desc;
+		containers[index].appendChild(newImg);
+
+		//Sets the data attribute values of the next/previous buttons
+		previousBtn.setAttribute('data-previous', Number(order) - 1);
+		nextBtn.setAttribute('data-next', Number(order) + 1);
+
+		//Repositions the viewport to the active image
+		var translateValue = 'translateX(' + (scrapbookImages[index].getAttribute('data-order') - 1) * -100 + '%)';
+		modalImageContainer.style.transform = translateValue;
+
+		//Sets the title and description of the active image in the side content area
+		currentImageTitle.innerHTML = title;
+		currentImageDesc.innerHTML = desc;
+
+		//Active classes added for presentation
+		modal.classList.add('modal-active');
+		body.classList.add('no_scroll');
+
 	}
 
 
@@ -52,12 +113,12 @@
 	*
 	******************************************************/
 
-	var zoomBtn = document.getElementById('js-modal_zoom_in');
+	/*var zoomBtn = document.getElementById('js-modal_zoom_in');
 
 	zoomBtn.addEventListener('click', function(ev){
 		ev.preventDefault();
 		modalMainContent.classList.toggle('modal_main_content-zoomed_in');
-	});
+	});*/
 
 
 	/******************************************************
@@ -66,8 +127,79 @@
 	*
 	******************************************************/
 
-	var forwardBtn = document.getElementById('js-modal_main_content-forward_button'),
-		backBtn = document.getElementById('js-modal_main_content-back_button');
+	var forwardBtn = document.getElementById('js-modal_main_content-next_button'),
+		backBtn = document.getElementById('js-modal_main_content-previous_button');
+
+	forwardBtn.addEventListener('click', goForward, false);
+	backBtn.addEventListener('click', goBack, false);
+
+
+
+	function goForward(){
+
+		var nextNo = Number( forwardBtn.getAttribute('data-next') );
+
+		if (nextNo > scrapbookImagesTotal){
+			return;
+		}
+
+		console.log( nextNo );
+		console.log( scrapbookImagesTotal );
+
+
+		//var desc = scrapbookImages[nextNo].getAttribute('data-desc'),
+		var	containers = document.getElementsByClassName('modal_img_container');
+
+
+		var translateValue = 'translateX(' + (((nextNo - 1) * 100) * -1) + '%)';
+		modalImageContainer.style.transform = translateValue;
+
+
+		previousBtn.setAttribute( 'data-previous', Number(nextNo) - 1 );
+		nextBtn.setAttribute( 'data-next', Number(nextNo) + 1 );
+
+		//Only insert Image tag if the container is empty
+		if(containers[nextNo - 1].innerHTML === ""){
+
+			//Injects the active image into the DOM 
+			var newImg = document.createElement('img');
+			newImg.classList.add('modal_main_img');
+			newImg.src = "img/scrapbook/image"+ nextNo + ".jpg";
+			//newImg.alt = desc;
+			containers[nextNo - 1].appendChild(newImg);
+
+		}
+
+
+
+
+
+		/*
+
+		update data attributes of forward / back buttons
+		translate container to next image
+		inject image into container
+		update title and desc
+
+
+		*/
+	}
+
+	function goBack(){
+		
+		var previousNo = Number( backBtn.getAttribute('data-previous') );
+
+		if( previousNo === 0 ){
+			return;
+		}
+
+		var translateValue = 'translateX(' + (((previousNo - 1) * 100) * -1) + '%)';
+		modalImageContainer.style.transform = translateValue;
+
+		previousBtn.setAttribute( 'data-previous', Number(previousNo) - 1 );
+		nextBtn.setAttribute( 'data-next', Number(previousNo) + 1 );
+
+	}
 
 	/*var scrapbookClose = document.getElementById('js-close_scrapbook_container'),
 		scrapbookContainer = document.getElementById('js-scrapbook_container'),
